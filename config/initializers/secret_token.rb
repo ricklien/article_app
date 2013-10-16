0b311c8f07970d462d4a939fa4e84a118e2b070e
ricklien <ricklien@gmail.com>
2013-10-16 15:40:25 -0700

    Scaffolding & Twitter Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArticleApp::Application.config.secret_key_base = '1548197be4e8cb1ce04a73b1ad48d1d511b1ed310032e47fb7c0e3208b06a474a3a2e4963c80118abcf0a55d54201c3614988cf251664b856440cc932e814b24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArticleApp::Application.config.session_store :cookie_store, key: '_article_app_session'
